64739f89d2e2c27556a58780e5a2f1ec267e4bd2
Art <amolc@pm.me>
2023-08-02 20:24:44 +0100

    add cargo-rdme checks

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! [![MASTER CI status](https://github.com/{{github_name}}/{{crate_name}}-rs/actions/workflows/ci.yml/badge.svg)](https://github.com/{{github_name}}/{{crate_name}}-rs/actions/workflows/ci.yml?query=branch%3Amaster)
+//! [![crates.io badge](https://img.shields.io/crates/v/{{crate_name}})](https://crates.io/crates/{{crate_name}})
+//! [![docs.rs badge](https://img.shields.io/docsrs/{{crate_name}}?label=docs.rs)](https://docs.rs/{{crate_name}})
+//! [![dependencies badge](https://img.shields.io/librariesio/release/cargo/{{crate_name}})](https://libraries.io/cargo/{{crate_name}})
+
 #![deny(clippy::correctness, clippy::suspicious)]
 #![warn(clippy::complexity, clippy::perf, clippy::style, clippy::pedantic)]
 
